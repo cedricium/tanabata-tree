43520ab80eef688115be45eb9373fecee198c4b8
Cedricium <amaya.cedric@gmail.com>
2017-07-23 23:16:46 -0700

    Created frontend webpage for all/ API route

---
+++ frontend/all/all.js
@@ -0,0 +1,57 @@
+//const getBtn = document.getElementById('get');
+//getBtn.addEventListener('click', () => {
+//  // making GET request
+//  let xml = new XMLHttpRequest();
+//  xml.addEventListener('load', requestListener);
+//  xml.open('GET', '../all');
+//  xml.send();
+//  
+//  function requestListener() {
+//    let data = this.responseText;
+//    let tanzakus = JSON.parse(data);
+//    
+//    let keys = Object.keys(tanzakus);
+//    for (let i = 0; i < keys.length; i++) {
+//      let title = keys[i];
+//      let url = tanzakus[title];
+//      
+//      fillTable(title, url);
+//    }
+//  }
+//});
+
+document.addEventListener('DOMContentLoaded', () => {
+  // making GET request
+  let xml = new XMLHttpRequest();
+  xml.addEventListener('load', requestListener);
+  xml.open('GET', '../all');
+  xml.send();
+  
+  function requestListener() {
+    let data = this.responseText;
+    let tanzakus = JSON.parse(data);
+    
+    let keys = Object.keys(tanzakus);
+    for (let i = 0; i < keys.length; i++) {
+      let title = keys[i];
+      let url = tanzakus[title];
+      
+      fillTable(title, url);
+    }
+  }
+});
+
+function fillTable(title, url) {
+  const table = document.getElementById('table');
+  
+  let row = table.insertRow(1);
+  let cellTitle = row.insertCell(0);
+  let cellUrl = row.insertCell(1);
+  
+  let anchor = document.createElement('a');
+  anchor.setAttribute('href', url);
+  anchor.innerHTML = url;
+  
+  cellTitle.innerHTML = title;
+  cellUrl.appendChild(anchor);
+}
