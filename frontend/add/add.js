a22b6d11a8f4dc312155af525206fedb8a8a829c
Cedricium <amaya.cedric@gmail.com>
2017-07-24 16:17:01 -0700

    Added button functionality in regards to server response

--- frontend/add/add.js
+++ frontend/add/add.js
@@ -6,7 +6,11 @@ btnRemove.addEventListener('click', () => {
 });
 
 const btnAdd = document.getElementById('add');
-btnAdd.addEventListener('click', () => {
+btnAdd.addEventListener('click', submit)
+
+function submit() {
+  btnAdd.classList.add('is-loading');
+  
   let title = document.getElementById('title').value;
   let url = document.getElementById('url').value;
   
@@ -16,6 +20,7 @@ btnAdd.addEventListener('click', () => {
   // error handling (checking for empty values)
   if (title.length === 0 || url.length === 0) {
     errorNotif.style.visibility = 'visible';
+    btnAdd.classList.remove('is-loading');
     return false;
   }
   
@@ -26,6 +31,25 @@ btnAdd.addEventListener('click', () => {
   xml.send();
   
   function requestListener() {
-    console.log(this.responseText);
+    let data = this.responseText;
+    let response = JSON.parse(data);
+    console.log(response);
+    
+    responseHandler(response);
   }
-});
+}
+
+function responseHandler(response) {
+  if (response.status === 'success') {
+    btnAdd.classList.remove('is-loading');
+    
+    // change button to direct to tanzakus list
+    btnAdd.innerHTML = 'Success! Click again to view tanzakus.';
+    btnAdd.removeEventListener('click', submit);
+    btnAdd.addEventListener('click', viewTanzakus);
+  }
+}
+
+function viewTanzakus() {
+  window.location.replace('../all/tanzakus.html');
+}
