d330d7378acbf36469b8225d987ddd521c9b0b4f
Cedricium <amaya.cedric@gmail.com>
2017-11-16 12:47:57 -0800

    Updated database query to check if table exists
    Hopefully this helps with closing #5.

--- server/models/database.js
+++ server/models/database.js
@@ -4,12 +4,23 @@ const connectionString = process.env.DATABASE_URL;
 const client = new pg.Client(connectionString);
 
 client.connect();
-client.query('CREATE TABLE IF NOT EXISTS tanabata_tree (id uuid, url VARCHAR(2000), \
-  title TEXT, description TEXT, been_visited BOOLEAN DEFAULT false, created_at TIMESTAMP DEFAULT NOW())', (err, res) => {
-    if (err) {
-      client.end();
-      return console.error('error with PostgreSQL database', err);
-    }
+
+client.query("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'tanabata_tree');", (err, res) => {
+  let tableExists = '';
+  if (err)
+    return console.error(err);
+  tableExists = JSON.stringify(res.rows[0].exists);
+  if (tableExists === 'false') {
+    client.query('CREATE TABLE tanabata_tree (id uuid, url VARCHAR(2000), \
+      title TEXT, description TEXT, been_visited BOOLEAN DEFAULT false, created_at TIMESTAMP DEFAULT NOW())', (err, res) => {
+        if (err) {
+          client.end();
+          return console.error('error with PostgreSQL database', err);
+        }
+
+        console.log(res);
+    });
+  }
 });
 
 client.end();
